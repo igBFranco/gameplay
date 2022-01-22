550261689913f509fef2568a098dfa14513ceb8e
igBFranco <igor.franco@aluno.unc.br>
2022-01-22 10:18:16 -0300

    Adicionando Routes SignIn e Home

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhan
 import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import {View } from 'react-native';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 import { Background } from './src/components/Background';
 
 export default function App() {
@@ -24,7 +24,7 @@ export default function App() {
   return (
     <Background>
       <StatusBar style="light" />
-      <Home></Home>
+      <Routes />
     </Background>
   );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,8 +4,18 @@ import { StatusBar } from "expo-status-bar";
 import { styles } from './styles';
 import IllustrationImg from '../../assets/illustration.png';
 import { ButtonIcon } from '../../components/ButtonIcon';
+import { useNavigation } from "@react-navigation/native";
+
+
 
 export function SignIn() {
+
+  const navigation = useNavigation();
+
+  function handleSignIn(){
+    navigation.navigate('Home');
+  }
+
 
   return (
     <View style={styles.container}>
@@ -20,7 +30,11 @@ export function SignIn() {
             Crie grupos para jogar seus games {'\n'}
             favoritos com seus amigos
         </Text>
-        <ButtonIcon title="Entrar com Discord" activeOpacity={0.7}></ButtonIcon>
+        <ButtonIcon 
+          title="Entrar com Discord" 
+          onPress={handleSignIn}
+        >
+        </ButtonIcon>
       </View>
 
     </View>
