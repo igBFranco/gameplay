f87661a0de9289d811f8ee75e56fcf4ed12b9845
igBFranco <igor.franco@aluno.unc.br>
2022-01-22 15:07:48 -0300

    Adicionando Lista de Agendamento

---
+++ src/components/GuildIcon/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Image } from "react-native";
+import { styles } from "./styles";
+
+
+
+export function GuildIcon() {
+    const uri = 'https://i.imgur.com/DvpvklR.png';
+
+    return(
+        <Image 
+            source={{ uri }} 
+            style={styles.image}
+            resizeMode="cover"    
+        />   
+        
+    );
+}
