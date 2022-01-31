ad1910c8fdd502812bac71e14474c1a1509c33a8
igBFranco <igor.franco@aluno.unc.br>
2022-01-31 16:28:32 -0300

    Adicionando Dados do Usuário

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { useAuth } from "../../hooks/auth";
 import { Avatar } from "../Avatar";
 import { styles } from "./styles";
 
 export function Profile() {
+    const { user } = useAuth();
     return(
         <View style={styles.container}>
 
-            <Avatar urlImage="https://github.com/igBFranco.png"></Avatar>
+            <Avatar urlImage={user.avatar}/>
             <View>
                 <View style={styles.user}>
                     <Text style={styles.greeting}>
                         Olá,
                     </Text>
                     <Text style={styles.username}>
-                        Igor
+                        {user.firstName}
                     </Text>
                 </View>
                 <Text style={styles.message}>
